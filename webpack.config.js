f35b064fa578b3c4407f0f2bdb621d145651b9a6
Kyle Ramirez <kyletramirez@me.com>
2017-10-20 13:23:20 -0500

    Name change disabled hotloading

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     ]
   },
   plugins: [
+    production ? null : new webpack.NamedModulesPlugin(),
     production ? null : new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
